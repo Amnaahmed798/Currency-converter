2356f7af38cf6c788273fc0ecab6a8b4d10f282d
Amnaahmed798 <amnaahmed.9112@gmail.com>
2024-05-10 00:38:41 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blue.bold("\n\t Welcome to amnas-currency-converter\n"));
+let exchangeRate = {
+    "USD": 1.0, // US Dollar
+    "EUR": 0.83, // Euro
+    "GBP": 0.72, // British Pound
+    "JPY": 109.42, // Japanese Yen
+    "AUD": 1.29, // Australian Dollar
+    "CAD": 1.25, // Canadian Dollar
+    "PAK": 277.7,
+};
+let userAnswer = await inquirer.prompt([{
+        name: "fromCurrency",
+        type: "list",
+        message: "select currency to convert from ",
+        choices: ["USD", "EUR", "GBP", "JPY", "AUD", "CAD", "PAK"]
+    },
+    {
+        name: "toCurrency",
+        type: "list",
+        message: "select currency to convert to ",
+        choices: ["USD", "EUR", "GBP", "JPY", "AUD", "CAD", "PAK"]
+    },
+    {
+        name: "amount",
+        type: "input",
+        message: "enter amount to convert"
+    }]);
+let fromAmount = exchangeRate[userAnswer.fromCurrency];
+let toAmount = exchangeRate[userAnswer.toCurrency];
+let amount = userAnswer.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(`converted amount = ${convertedAmount}`);
---
+++ readme.md
@@ -0,0 +1,6 @@
+# "Simple Currency Converter." 
+
+"This project is a command-line tool that helps you convert currency values from one currency to another."
+ "To install, just clone this repository and run npm install."
+ "To convert currency, run node index.js followed by the currency you're converting from, the currency you're converting to, and the amount you want to convert."
+ Features: "Supports multiple currencies," "Updates exchange rates automatically," or "Handles errors gracefully."# Currency-converter
